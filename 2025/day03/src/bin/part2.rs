3fd4c0e9793bd9af17585303d6d6cafc5b4bc4d3
Antoni Guss <antoniguss0@gmail.com>
2025-12-03 10:22:04 +0100

    solved 2025 day03 part2

--- 2025/day03/src/bin/part2.rs
+++ 2025/day03/src/bin/part2.rs
@@ -1,13 +1,37 @@
 fn main() {
-    let input = include_str!("input2.txt");
+    let input = include_str!("input1.txt");
 
     let result = part2(input);
 
     dbg!(&result);
 }
 
-fn part2(input: &str) -> i32 {
-    0
+fn part2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|l| {
+            let bank: Vec<u32> = l.chars().map(|c| c.to_digit(10).unwrap()).collect();
+            find_joltage(&bank)
+        })
+        .sum()
+}
+
+fn find_joltage(bank: &[u32]) -> u64 {
+    let mut result: u64 = 0;
+    let mut last = 0;
+
+    for rem in (0..12).rev() {
+        let max_val = bank[last..bank.len() - rem].iter().max().unwrap();
+        let index = bank[last..bank.len() - rem]
+            .iter()
+            // Returns first index of max_val
+            .position(|&x| x == *max_val)
+            .unwrap();
+        last = last + index + 1;
+        result = result * 10 + (*max_val as u64);
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -16,8 +40,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part2_works() {
-        let result = part2("");
-        assert_eq!(result, 0)
+    fn example_works() {
+        let result = part2(
+            "987654321111111
+811111111111119
+234234234234278
+818181911112111",
+        );
+        assert_eq!(result, 3121910778619);
+    }
+
+    #[test]
+    fn parts_work() {
+        let bank: Vec<u32> = "987654321111111"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+        assert_eq!(find_joltage(&bank), 987654321111);
+
+        let bank: Vec<u32> = "811111111111119"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+        assert_eq!(find_joltage(&bank), 811111111119);
+
+        let bank: Vec<u32> = "234234234234278"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+        assert_eq!(find_joltage(&bank), 434234234278);
+
+        let bank: Vec<u32> = "818181911112111"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+        assert_eq!(find_joltage(&bank), 888911112111);
     }
 }
